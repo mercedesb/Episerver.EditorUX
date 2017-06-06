31e6fddad7777414c42e5019c86d2285b51f6a71
mercedesb <mercedesbernard@live.com>
2017-06-05 19:01:52 -0500

    required prop indicator

---
+++ EditorUX/Models/Metadata/RequiredPropertiesMetadataExtender.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Cms.Shell.UI.ObjectEditing;
+using EPiServer.Shell.ObjectEditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EditorUX.Models.Metadata
+{
+    public class RequiredPropertiesMetadataExtender : IMetadataExtender
+    {
+        public void ModifyMetadata(ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            // Add asterisk to required (editable) properties
+            IEnumerable<ContentDataMetadata> props = metadata.Properties.OfType<ContentDataMetadata>().Where(p => p.IsRequired && !p.IsReadOnly);
+
+            foreach (var property in props)
+            {
+                property.DisplayName = $"{property.DisplayName} (*)";
+            }
+        }
+    }
+}
